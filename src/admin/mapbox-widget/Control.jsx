f515903a1254a4f84c98a8350f64be7ba8e3e010
okdistribute <633012+okdistribute@users.noreply.github.com>
2020-04-18 15:19:07 -0700

    fix: mapbox

--- src/admin/mapbox-widget/Control.jsx
+++ src/admin/mapbox-widget/Control.jsx
@@ -1,47 +1,89 @@
 import PropTypes from 'prop-types'
-import React from 'react'
+import ReactMapboxGl from 'react-mapbox-gl'
+import React, { useCallback, useState } from 'react'
 import mapboxgl from 'mapbox-gl'
+import { fromJS } from 'immutable'
 
 var propTypes = {
   onChange: PropTypes.func.isRequired,
   forID: PropTypes.string,
-  value: PropTypes.node,
+  value: PropTypes.any,
   classNameWrapper: PropTypes.string.isRequired
 }
 
 var defaultProps = {
-  value: ''
+  value: {
+    style: '',
+    layers: []
+  }
 }
 mapboxgl.accessToken = 'pk.eyJ1IjoiYWxpeWEiLCJhIjoiY2lzZDVhbjM2MDAwcTJ1cGY4YTN6YmY4cSJ9.NxK9jMmYZsA32ol_IZGs5g'
 
-class Control extends React.Component {
-  componentWillMount () {
+function Control ({ forID, value, onChange, classNameWrapper }) {
+  const [ styleURL, setStyle ] = useState(value.style)
+
+  function _onStyleLoad (map) {
+    const _style = map.getStyle()
+    if (!_style) onChange(fromJS({ style: styleURL, layers: [] }))
+    if (_style.layers) {
+      var val = fromJS({ style: styleURL, layers: _style.layers })
+      onChange(val)
+      console.log('onChange', val)
+    }
   }
 
-  render () {
-    const {
-      forID,
-      value,
-      onChange,
-      classNameWrapper
-    } = this.props
+  const onStyleLoad = useCallback(_onStyleLoad, [styleURL])   
 
-    return (
-      <div>
-        <input
-          type='text'
-          id={forID}
-          placeholder='StyleURL'
-          className={classNameWrapper}
-          value={value || ''}
-          onChange={e => onChange(e.target.value)}
-        />
-      </div>
-    )
-  }
+  return (
+    <div>
+      <input
+        type='text'
+        id={forID}
+        placeholder='StyleURL'
+        className={classNameWrapper}
+        value={styleURL || ''}
+        onChange={e => setStyle(e.target.value)}
+      />
+      {styleURL && <MapPreview style={styleURL} onStyleLoad={onStyleLoad} />}
+    </div>
+  )
 }
 
 Control.defaultProps = defaultProps
 Control.propTypes = propTypes
 
 export default Control
+
+const MapPreview = React.memo(function ({ style, onStyleLoad }) {
+  // Netlify-CMS Preview elements are rendered in an iframe
+  // This hacks in the mapbox-gl css, because simply importing 
+  // in this file will not work 
+  // const iframe = document.querySelector('iframe')
+  // const head = iframe.contentDocument.documentElement.querySelector('head')
+  // const link = document.createElement('link')
+  // link.setAttribute('href', 'https://api.mapbox.com/mapbox-gl-js/v1.9.1/mapbox-gl.css')
+  // link.setAttribute('rel', 'stylesheet')
+  // head.appendChild(link)
+
+  const Map = ReactMapboxGl({
+    accessToken:
+      'pk.eyJ1IjoiYWxpeWEiLCJhIjoiY2lzZDVhbjM2MDAwcTJ1cGY4YTN6YmY4cSJ9.NxK9jMmYZsA32ol_IZGs5g',
+    hash: false,
+    doubleClickZoom: false,
+    logoPosition: 'bottom-right'
+  })
+
+  var defaultCenter = [-79.656232, -0.489971]
+
+  return (
+    <Map onStyleLoad={onStyleLoad}
+      center={defaultCenter}
+      zoom={[6]}
+      style={style}
+      containerStyle={{
+        height: '0',
+        width: '0'
+      }}>
+    </Map>
+  )
+})
--- src/admin/mapbox-widget/Preview.jsx
+++ src/admin/mapbox-widget/Preview.jsx
@@ -1,60 +1,19 @@
-import ReactMapboxGl, { ScaleControl } from 'react-mapbox-gl'
-import React, { useState, useCallback } from 'react'
+import PropTypes from 'prop-types'
+import React from 'react'
 
-export default function Preview ({ value }) {
-  const [layers, setLayers] = useState([])
-  if (!value) return <div>Requires style url</div>
-
-  function _onStyleLoad (map) {
-    const style = map.getStyle()
-    if (!style) setLayers([])
-    if (style.layers) setLayers(style.layers)
-  }
-
-  const onStyleLoad = useCallback(_onStyleLoad, [])   
-
+function Preview ({ value }) {
+  console.log(value)
+  if (!value || !value.layers) return <div></div>
   return (
     <div>
-      {layers.map((layer) => {
-        console.log(layer)
+      {value.layers && value.layers.map((layer) => {
         return <div>{layer.id}</div>
       })}
-      <MapPreview style={value} onStyleLoad={onStyleLoad} />
     </div>
   )
 }
 
-const MapPreview = React.memo(function ({ style, onStyleLoad }) {
-  // Netlify-CMS Preview elements are rendered in an iframe
-  // This hacks in the mapbox-gl css, because simply importing it 
-  // in this file will not work 
-  const iframe = document.querySelector('iframe')
-  const head = iframe.contentDocument.documentElement.querySelector('head')
-  const link = document.createElement('link')
-  link.setAttribute('href', 'https://api.mapbox.com/mapbox-gl-js/v1.9.1/mapbox-gl.css')
-  link.setAttribute('rel', 'stylesheet')
-  head.appendChild(link)
-
-  const Map = ReactMapboxGl({
-    accessToken:
-      'pk.eyJ1IjoiYWxpeWEiLCJhIjoiY2lzZDVhbjM2MDAwcTJ1cGY4YTN6YmY4cSJ9.NxK9jMmYZsA32ol_IZGs5g',
-    hash: false,
-    doubleClickZoom: false,
-    logoPosition: 'bottom-right'
-  })
-
-  var defaultCenter = [-79.656232, -0.489971]
-
-  return (
-    <Map onStyleLoad={onStyleLoad}
-      center={defaultCenter}
-      zoom={[6]}
-      style={style}
-      containerStyle={{
-        height: '0',
-        width: '0'
-      }}>
-      <ScaleControl position={'top-right'} />
-    </Map>
-  )
-})
+Preview.propTypes = {
+  value: PropTypes.any
+}
+export default Preview
