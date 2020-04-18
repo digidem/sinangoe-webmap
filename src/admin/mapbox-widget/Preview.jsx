bf01f8340f1e04b456a2a02d3eb2f5f0f2f2cfec
okdistribute <633012+okdistribute@users.noreply.github.com>
2020-04-18 14:49:08 -0700

    feat: display layers from preview

--- src/admin/mapbox-widget/Preview.jsx
+++ src/admin/mapbox-widget/Preview.jsx
@@ -1,34 +1,60 @@
-import PropTypes from 'prop-types'
 import ReactMapboxGl, { ScaleControl } from 'react-mapbox-gl'
-import 'mapbox-gl/dist/mapbox-gl.css'
-import * as React from 'react'
+import React, { useState, useCallback } from 'react'
 
 export default function Preview ({ value }) {
+  const [layers, setLayers] = useState([])
   if (!value) return <div>Requires style url</div>
-  var defaultCenter = [ -79.656232, -0.489971 ]
+
+  function _onStyleLoad (map) {
+    const style = map.getStyle()
+    if (!style) setLayers([])
+    if (style.layers) setLayers(style.layers)
+  }
+
+  const onStyleLoad = useCallback(_onStyleLoad, [])   
+
+  return (
+    <div>
+      {layers.map((layer) => {
+        console.log(layer)
+        return <div>{layer.id}</div>
+      })}
+      <MapPreview style={value} onStyleLoad={onStyleLoad} />
+    </div>
+  )
+}
+
+const MapPreview = React.memo(function ({ style, onStyleLoad }) {
+  // Netlify-CMS Preview elements are rendered in an iframe
+  // This hacks in the mapbox-gl css, because simply importing it 
+  // in this file will not work 
+  const iframe = document.querySelector('iframe')
+  const head = iframe.contentDocument.documentElement.querySelector('head')
+  const link = document.createElement('link')
+  link.setAttribute('href', 'https://api.mapbox.com/mapbox-gl-js/v1.9.1/mapbox-gl.css')
+  link.setAttribute('rel', 'stylesheet')
+  head.appendChild(link)
 
   const Map = ReactMapboxGl({
-    accessToken: 'pk.eyJ1IjoiYWxpeWEiLCJhIjoiY2lzZDVhbjM2MDAwcTJ1cGY4YTN6YmY4cSJ9.NxK9jMmYZsA32ol_IZGs5g',
+    accessToken:
+      'pk.eyJ1IjoiYWxpeWEiLCJhIjoiY2lzZDVhbjM2MDAwcTJ1cGY4YTN6YmY4cSJ9.NxK9jMmYZsA32ol_IZGs5g',
     hash: false,
     doubleClickZoom: false,
     logoPosition: 'bottom-right'
   })
 
+  var defaultCenter = [-79.656232, -0.489971]
+
   return (
-    <Map
+    <Map onStyleLoad={onStyleLoad}
       center={defaultCenter}
       zoom={[6]}
-      style={value}
+      style={style}
       containerStyle={{
-        height: '100vh',
-        width: '100vw'
+        height: '0',
+        width: '0'
       }}>
       <ScaleControl position={'top-right'} />
-
     </Map>
   )
-}
-
-Preview.propTypes = {
-  value: PropTypes.node
-}
+})
--- src/admin/mapbox-widget/index.js
+++ src/admin/mapbox-widget/index.js
@@ -1,6 +1,10 @@
 import Control from './Control'
 import Preview from './Preview'
 
+if (typeof window !== 'undefined') {
+  window.Control = Control
+  window.Preview = Preview
+}
 const Widget = {
   name: 'mapbox',
   controlComponent: Control,
