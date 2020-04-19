988f4c14fdaffdeb30411ff9601fcdf36547d983
okdistribute <633012+okdistribute@users.noreply.github.com>
2020-04-18 19:11:29 -0700

    add mapstyle to mapbox-config

--- mapbox-config.js
+++ mapbox-config.js
@@ -1,3 +1,4 @@
 module.exports = {
+  style: 'mapbox://styles/aliya/cjgowcgqq00a62spkeo922ik6',
   accessToken: 'pk.eyJ1IjoiYWxpeWEiLCJhIjoiY2lzZDVhbjM2MDAwcTJ1cGY4YTN6YmY4cSJ9.NxK9jMmYZsA32ol_IZGs5g'
 }
